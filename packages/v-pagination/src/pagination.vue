516985c424ee960b1795d921bf5b4808ebea772c
huangshuwei <hsw89520@163.com>
2017-02-09 16:16:17 +0800

    分页优化

--- packages/v-pagination/src/pagination.vue
+++ packages/v-pagination/src/pagination.vue
@@ -1,29 +1,32 @@
 <template>
-        <ul class="et-page-ul">
-            <span class="et-page-total">共 800 条</span>
+    <ul class="et-page-ul">
+        <span class="et-page-total">共 800 条</span>
 
-            <li :class="[pageIndex=== 1 ? 'et-page-disabled' :'','et-page-li','et-page-prev']"><a><i class="et-icon icon-angle-left"></i></a></li>
+        <li @click.stop.prevent="prevPage()" :class="[pageIndex=== 1 ? 'et-page-disabled' :'','et-page-li','et-page-prev']"><a><i
+                class="et-icon icon-angle-left"></i></a></li>
 
-            <li v-if="isShowFirstPage" class="et-page-li"><a>1</a></li>
+        <li @click.stop.prevent="jumpPage(1)" :class='[pageIndex === 1 ? "et-page-li-active":"","et-page-li"]'><a>1</a></li>
 
-            <li v-if="isShowFirstPage" :class="[pageIndex=== 1 ? 'disabled' :'','et-page-li','et-page-jump-prev']" :title="'向前 '+showPagingCount+' 页'">
-                <a><i class="et-icon icon-angle-double-left"></i></a>
-            </li>
+        <li @click.stop.prevent="jumpPage(pageIndex - showPagingCount)" v-if="showJumpPrev" :class="[pageIndex=== 1 ? 'disabled' :'','et-page-li','et-page-jump-prev']"
+            :title="'向前 '+showPagingCount+' 页'">
+            <a><i class="et-icon icon-angle-double-left"></i></a>
+        </li>
 
-            <li v-for="num in pagingCounts"  @click.stop.prevent="jumpPage(num)" class="et-page-li"><a>{{num}}</a></li>
-          <!--  <li class="et-page-li"><a>77</a></li>
-            <li class="et-page-li"><a>78</a></li>
-            <li class="et-page-li"><a>79</a></li>-->
-            <li class="et-page-li et-page-jump-next" title="向后 5 页"><a><i class="et-icon icon-angle-double-right"></i></a>
-            <li class="et-page-li"><a>80</a></li>
-            <li class="et-page-li et-page-next"><a><i class="et-icon icon-angle-right"></i></a></li>
+        <li v-for="num in pagingCounts" @click.stop.prevent="jumpPage(num)"
+            :class='[num === pageIndex ? "et-page-li-active":"","et-page-li"]'><a>{{num}}</a></li>
 
-            <v-select class="et-page-select"
-                      :labels="pageSizeOption"
-                      :currentLabel="pageSize"
-            ></v-select>
-            <span class="et-page-goto">前往 <input class="et-page-goto-input" type="input"></span> 页
-        </ul>
+        <li @click.stop.prevent="jumpPage(pageIndex + showPagingCount)" v-if="showJumpNext" class="et-page-li et-page-jump-next" :title="'向后 '+showPagingCount+' 页'"><a><i class="et-icon icon-angle-double-right"></i></a>
+
+        <li @click.stop.prevent="jumpPage(pageCount)" :class='[pageIndex === pageCount ? "et-page-li-active":"","et-page-li"]'><a>{{pageCount}}</a></li>
+
+        <li @click.stop.prevent="nextPage()" :class="[pageIndex=== pageCount ? 'et-page-disabled' :'','et-page-li','et-page-next']"><a><i class="et-icon icon-angle-right"></i></a></li>
+
+        <v-select class="et-page-select"
+                  :labels="pageSizeOption"
+                  :currentLabel="pageSize"
+        ></v-select>
+        <span class="et-page-goto">前往 <input class="et-page-goto-input" type="input"></span> 页
+    </ul>
 </template>
 
 <script>
@@ -31,10 +34,10 @@
 
     export default{
         name: 'v-pagination',
-        components:{
+        components: {
             VSelect
         },
-        props:{
+        props: {
             // 当前页
             pageIndex: {
                 type: Number,
@@ -48,12 +51,11 @@
             // 最多显示几个数字按钮
             showPagingCount: {
                 type: Number,
-                require: true,
                 default: 5
             },
 
             // 每页大小
-            pageSize:{
+            pageSize: {
                 type: Number,
                 default: 10
             },
@@ -73,59 +75,59 @@
             }
         },
         computed: {
-            // 是否显示第一页按钮
-            isShowFirstPage(){
-                var vm = this
 
-                // 总页数<=要显示的数字 || 当前页数<= 要显示数字/2+1
-                if (vm.pageCount <= vm.showPagingCount || vm.pageIndex <= Math.floor(vm.showPagingCount / 2) + 1) {
-                    return false
-                }
-                return true
+            numOffset(){
+                return Math.floor((this.showPagingCount+2) / 2) - 1;
             },
 
-            // 是否显示最后一页按钮
-            isShowLastPage(){
-                var vm = this
-
-                // 总页数 > 要显示的数字 && 总页数 > 当前页数+要显示数字/2+1
-                if (vm.pageCount > vm.showPagingCount && vm.pageCount > vm.pageIndex + Math.floor(vm.showPagingCount / 2)) {
-                    return true
+            showJumpPrev(){
+                if (this.pageCount > this.showPagingCount + 2) {
+                    if (this.pageIndex > this.showPagingCount) {
+                        return true
+                    }
                 }
                 return false
             },
 
+            showJumpNext(){
+                if (this.pageCount > this.showPagingCount + 2) {
+                    if (this.pageIndex < this.pageCount - this.numOffset) {
+                        return true
+                    }
+                }
+                return false
+            },
+
+
             // 当前要显示的数字按钮集合
             pagingCounts(){
-                var vm = this,
-                    startNum, endNum,
-                    result = []
+                let vm = this,
+                    startNum,
+                    result = [],
+                    showJumpPrev = vm.showJumpPrev,
+                    showJumpNext = vm.showJumpNext;
 
 
-                // 总页数<=要显示的数字 || 当前页数<= 要显示数字/2+1
-                if (!vm.isShowFirstPage) {
-
-                    startNum = 1
-                    endNum = vm.pageCount < vm.showPagingCount ? vm.pageCount : vm.showPagingCount
-                    for (var i = startNum; i <= endNum; i++) {
-                        result.push(i)
+                if (showJumpPrev && !showJumpNext) {
+                    startNum = vm.pageCount - vm.showPagingCount;
+                    for (let i = startNum; i < vm.pageCount; i++) {
+                        result.push(i);
                     }
-
+                }else if (!showJumpPrev && showJumpNext) {
+                    for (let i = 2; i < vm.showPagingCount+2; i++) {
+                        result.push(i);
+                    }
+                } else if (showJumpPrev && showJumpNext) {
+                    /*let offset = Math.floor((vm.showPagingCount+2) / 2) - 1;*/
+                    for (let i = vm.pageIndex - vm.numOffset ; i <= vm.pageIndex + vm.numOffset; i++) {
+                        result.push(i);
+                    }
                 } else {
-
-                    startNum = vm.pageIndex - Math.floor(vm.showPagingCount / 2)
-                    endNum = vm.pageIndex + Math.floor(vm.showPagingCount / 2)
-
-                    // endNum 不能大于总页数
-                    endNum = endNum > vm.pageCount ? vm.pageCount : endNum
-                    // startNum 不能大于 总页数-显示的页数
-                    startNum = startNum > (vm.pageCount - vm.showPagingCount) ? (vm.pageCount - vm.showPagingCount + 1) : startNum
-
-                    for (var i = startNum; i <= endNum; i++) {
-                        result.push(i)
+                    for (let i = 2; i < vm.pageCount; i++) {
+                        result.push(i);
                     }
-
                 }
+
                 return result
             }
         },
@@ -134,6 +136,7 @@
             jumpPage(pageIndex){
                 this.$emit('pagingCallBack', pageIndex)
             },
+
 
             // 上一页
             prevPage(){
@@ -155,26 +158,11 @@
             changePageSize(pageSize){
                 this.pageSize = pageSize
                 this.$emit('pagingCallBack', 1, pageSize)
-            },
-
-            // 每页大小选择
-            pageSizeTrigger(){
-                var vm = this
-
-                vm.pageSizeClass = vm.pageSizeClass === 'open' ? '' : 'open'
-            },
-            // 焦点离开
-            pageSizeBlur(){
-                var vm = this
-
-                setTimeout(function () {
-                    vm.pageSizeClass = ''
-                }, 100)
-
             }
+
         },
         mounted(){
-            console.log(this.pagingCounts)
+
         }
 
     }
