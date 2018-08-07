98dc68a242b80a0ae386c816c49e606e0b27cd4d
Jens <jiangcs@qdhlm.cn>
2018-08-07 16:57:50 +0800

    添加右固定操作列： 父组件传入prop:actions以及doAction方法-build all

--- libs/v-table/src/scroll-control-mixin.js
+++ libs/v-table/src/scroll-control-mixin.js
@@ -14,21 +14,29 @@ exports.default = {
             methods: {
                         body1Mousewheel: function body1Mousewheel(e) {
 
+                                    var body1 = this.$el.querySelector('.v-table-leftview .v-table-body');
                                     var body2 = this.$el.querySelector('.v-table-rightview .v-table-body');
+                                    var body3 = this.$el.querySelector('.v-table-actionview .v-table-body');
 
                                     var e1 = e.originalEvent || window.event || e;
                                     var scrollHeight = e1.wheelDelta || e1.detail * -1;
+                                    body1.scrollTop = body1.scrollTop - scrollHeight;
                                     body2.scrollTop = body2.scrollTop - scrollHeight;
+                                    body3.scrollTop = body3.scrollTop - scrollHeight;
                         },
                         bodyScrollTop: function bodyScrollTop() {
 
                                     var body1 = this.$el.querySelector('.v-table-leftview .v-table-body');
                                     var body2 = this.$el.querySelector('.v-table-rightview .v-table-body');
+                                    var body3 = this.$el.querySelector('.v-table-actionview .v-table-body');
 
                                     if (body1) {
                                                 body1.scrollTop = 0;
                                     }
-                                    body2.scrollTop = 0;
+                                    if (body2) {
+                                                body2.scrollTop = 0;
+                                    }
+                                    body3.scrollTop = 0;
                         },
                         body2Scroll: function body2Scroll(e) {
 
@@ -41,6 +49,18 @@ exports.default = {
                                     }
 
                                     view2.querySelector('.v-table-header').scrollLeft = body2.scrollLeft;
+                        },
+                        body3Scroll: function body3Scroll() {
+                                    var body1 = this.$el.querySelector('.v-table-leftview .v-table-body');
+                                    var body2 = this.$el.querySelector('.v-table-rightview .v-table-body');
+                                    var body3 = this.$el.querySelector('.v-table-actionview .v-table-body');
+
+                                    if (body1) {
+                                                body1.scrollTop = body3.scrollTop;
+                                    }
+                                    if (body2) {
+                                                body2.scrollTop = body3.scrollTop;
+                                    }
                         },
                         rightViewFooterScroll: function rightViewFooterScroll() {
 
@@ -61,10 +81,14 @@ exports.default = {
                                                 var body1 = _this.$el.querySelector('.v-table-leftview .v-table-body');
                                                 var body2 = _this.$el.querySelector('.v-table-rightview .v-table-body');
                                                 var rightViewFooter = _this.$el.querySelector('.v-table-rightview .v-table-footer');
+                                                var body3 = _this.$el.querySelector('.v-table-actionview .v-table-body');
 
-                                                _utils2.default.bind(body1, 'mousewheel', _this.body1Mousewheel);
+                                                _utils2.default.bind(body1, 'mousewheel', _this.bodyMousewheel);
+                                                _utils2.default.bind(body2, 'mousewheel', _this.bodyMousewheel);
+                                                _utils2.default.bind(body3, 'mousewheel', _this.bodyMousewheel);
                                                 _utils2.default.bind(body2, 'scroll', _this.body2Scroll);
                                                 _utils2.default.bind(rightViewFooter, 'scroll', _this.rightViewFooterScroll);
+                                                _utils2.default.bind(body3, 'scroll', _this.body3Scroll);
                                     });
                         },
                         unbindEvents: function unbindEvents() {
@@ -72,10 +96,14 @@ exports.default = {
                                     var body1 = this.$el.querySelector('.v-table-leftview .v-table-body');
                                     var body2 = this.$el.querySelector('.v-table-rightview .v-table-body');
                                     var rightViewFooter = this.$el.querySelector('.v-table-rightview .v-table-footer');
+                                    var body3 = this.$el.querySelector('.v-table-actionview .v-table-body');
 
-                                    _utils2.default.unbind(body1, 'mousewheel', this.body1Mousewheel);
+                                    _utils2.default.unbind(body1, 'mousewheel', this.bodyMousewheel);
+                                    _utils2.default.unbind(body2, 'mousewheel', this.bodyMousewheel);
+                                    _utils2.default.unbind(body3, 'mousewheel', this.bodyMousewheel);
                                     _utils2.default.unbind(body2, 'scroll', this.body2Scroll);
                                     _utils2.default.unbind(rightViewFooter, 'scroll', this.rightViewFooterScroll);
+                                    _utils2.default.unbind(body3, 'scroll', this.body3Scroll);
                         },
                         scrollToTop: function scrollToTop() {
 
