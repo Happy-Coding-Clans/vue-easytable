2ef5d6ce89ea589bb0c1ed8a402cc9c78dc52ccd
huangshuwei <hsw89520@163.com>
2021-01-28 10:11:50 +0800

    update

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,10 @@
 module.exports = {
     comments: false,
     plugins: ["transform-vue-jsx"],
-    /* presets: [["env", { modules: false }]], */
     env: {
         test: {
             presets: [["env", { targets: { node: "current" } }]],
-            plugins: ["istanbul"]
+            /* plugins: ["istanbul"] */
         }
     }
 };
