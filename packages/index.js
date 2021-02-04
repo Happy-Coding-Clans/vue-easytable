61747203745300f5b135a1460a730ea8e20ba300
huangshuwei <hsw89520@163.com>
2021-02-04 19:29:27 +0800

    revert

--- packages/index.js
+++ packages/index.js
@@ -13,7 +13,7 @@ import VeSelect from './ve-select';
 import VeTable from './ve-table';
 
 
-const version = '2.0.2';
+const version = '2.0.1';
 const components = [
   VeCheckbox,
   VeCheckboxGroup,
