22dcfad640a504d87b40905add0df07680ac79ea
huangshuwei <hsw89520@163.com>
2021-02-06 15:50:56 +0800

    1、Add Vue.observable() for explicitly creating observable objects 2、Specifies the Vue version dependency

--- packages/ve-table/src/util/store.js
+++ packages/ve-table/src/util/store.js
@@ -1,20 +1,17 @@
-/*
-暂未使用
-*/
-
-
 import Vue from "vue";
 
 // store
 export const store = Vue.observable({
-    cloneTableData: [],
-    cloneColumns: []
+    /*
+    table view port width except scroll bar width
+    */
+    tableViewportWidth: 0
 });
 
 // mutation
 export const mutations = {
-    initStoreData({ cloneTableData, cloneColumns }) {
-        store.cloneTableData = cloneTableData;
-        store.cloneColumns = cloneColumns;
+    // set store
+    setStore(payload) {
+        Object.assign(store, payload);
     }
 };
