f80cb72a934981c539de7bf0b57c8cca27cd4319
huangshuwei <huangsw89520@163.com>
2021-08-09 19:52:46 +0800

    eslint + prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     env: {
         node: true,
     },
-    extends: ["plugin:vue/essential", "eslint:recommended"],
+    extends: ["plugin:vue/essential", "eslint:recommended", "prettier"],
     rules: {
         "no-console": "off",
         "no-debugger": "off",
