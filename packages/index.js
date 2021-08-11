07da24db859b121d774c801b5e976c0c9c83ef7c
huangshuwei <huangsw89520@163.com>
2021-08-11 20:45:45 +0800

    eslint+preitter

--- packages/index.js
+++ packages/index.js
@@ -1,71 +1,72 @@
 // This file is auto gererated by build/build-entry.js
 
-import Vue from "vue";
-import VeCheckbox from "./ve-checkbox";
-import VeCheckboxGroup from "./ve-checkbox-group";
-import VeDropdown from "./ve-dropdown";
-import VeIcon from "./ve-icon";
-import VeLoading from "./ve-loading";
-import VeLocale from "./ve-locale";
-import VePagination from "./ve-pagination";
-import VeRadio from "./ve-radio";
-import VeSelect from "./ve-select";
-import VeTable from "./ve-table";
+import Vue from 'vue';
+import VeCheckbox from './ve-checkbox';
+import VeCheckboxGroup from './ve-checkbox-group';
+import VeDropdown from './ve-dropdown';
+import VeIcon from './ve-icon';
+import VeLoading from './ve-loading';
+import VeLocale from './ve-locale';
+import VePagination from './ve-pagination';
+import VeRadio from './ve-radio';
+import VeSelect from './ve-select';
+import VeTable from './ve-table';
 
-const version = "2.5.6";
+
+const version = '2.5.7';
 const components = [
-    VeCheckbox,
-    VeCheckboxGroup,
-    VeDropdown,
-    VeIcon,
-    VeLoading,
-    VeLocale,
-    VePagination,
-    VeRadio,
-    VeSelect,
-    VeTable,
+  VeCheckbox,
+  VeCheckboxGroup,
+  VeDropdown,
+  VeIcon,
+  VeLoading,
+  VeLocale,
+  VePagination,
+  VeRadio,
+  VeSelect,
+  VeTable
 ];
 
-const install = (Vue) => {
-    components.forEach((Component) => {
-        Vue.use(Component);
-    });
+const install = Vue => {
+  components.forEach(Component => {
+    Vue.use(Component);
+  });
 
-    Vue.prototype.$veLoading = VeLoading;
-    Vue.prototype.$veLocale = VeLocale;
+  Vue.prototype.$veLoading = VeLoading;
+  Vue.prototype.$veLocale = VeLocale;
 };
 
 /* istanbul ignore if */
-if (typeof window !== "undefined" && window.Vue) {
-    install(window.Vue);
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue);
 }
 
 export {
-    install,
-    version,
-    VeCheckbox,
-    VeCheckboxGroup,
-    VeDropdown,
-    VeIcon,
-    VeLoading,
-    VeLocale,
-    VePagination,
-    VeRadio,
-    VeSelect,
-    VeTable,
+  install,
+  version,
+  VeCheckbox,
+  VeCheckboxGroup,
+  VeDropdown,
+  VeIcon,
+  VeLoading,
+  VeLocale,
+  VePagination,
+  VeRadio,
+  VeSelect,
+  VeTable
 };
 
 export default {
-    install,
-    version,
-    VeCheckbox,
-    VeCheckboxGroup,
-    VeDropdown,
-    VeIcon,
-    VeLoading,
-    VeLocale,
-    VePagination,
-    VeRadio,
-    VeSelect,
-    VeTable,
+  install,
+  version,
+  VeCheckbox,
+  VeCheckboxGroup,
+  VeDropdown,
+  VeIcon,
+  VeLoading,
+  VeLocale,
+  VePagination,
+  VeRadio,
+  VeSelect,
+  VeTable
 };
