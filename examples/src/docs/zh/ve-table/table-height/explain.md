a4be67a30bd1e9a9131cf6df6bdf708961d6d700
huangshuwei <huangsw89520@163.com>
2021-08-12 09:43:08 +0800

    add table height example

---
+++ examples/src/docs/zh/ve-table/table-height/explain.md
@@ -0,0 +1,5 @@
+:::tip
+1、表格高度可以设置固定值。如：`:max-height="500"`<br>
+2、表格高度可以设置动态值。如：`max-height="calc(100vh - 210px)"` 或者 `max-height="80%"`<br>
+
+:::
---
+++ examples/src/docs/zh/ve-table/table-height/main.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>列宽设置</h2>
+        <Explain />
+        <NoWidth />
+    </div>
+</template>
+<script>
+import Explain from "./explain.md";
+import NoWidth from "./no-width.md";
+
+export default {
+    name: "basic-main",
+    components: {
+        Explain,
+        NoWidth,
+    },
+};
+</script>
