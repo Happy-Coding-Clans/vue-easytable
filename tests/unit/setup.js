10fc16a031359eab139b4e6de0e06304acd13c12
huangshuwei <huangsw89520@163.com>
2021-08-24 12:08:19 +0800

    单元测试添加触发 ResizeObserver 的原型方法

--- tests/unit/setup.js
+++ tests/unit/setup.js
@@ -1,4 +1,16 @@
 import Vue from "vue";
+import { Wrapper } from "@vue/test-utils";
 import vueEasytable from "../../packages/index";
 
 Vue.use(vueEasytable);
+
+// add Wrapper prototype
+Object.assign(Wrapper.prototype, {
+    findResizeObserver() {
+        return this.findComponent({ name: "vue-dom-resize-observer" });
+    },
+    triggerResizeObserver({ width = 0, height = 0 }) {
+        const ob = this.findResizeObserver();
+        ob.vm.resizeListener({ width, height });
+    },
+});
