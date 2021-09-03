ffabcd46e7b7e562476cb30284247796932899b6
a.hodyko <saschka1993@mail.ru>
2021-09-03 13:22:44 +0300

    Revert "support ie11"

--- babel.config.js
+++ babel.config.js
@@ -5,13 +5,12 @@ module.exports = {
             "@babel/env",
             {
                 targets: {
-                    ie: "11",
                     edge: "17",
                     firefox: "60",
                     chrome: "67",
                     safari: "11.1",
                 },
-                useBuiltIns: "entry",
+                useBuiltIns: "usage",
                 corejs: "3.6.5",
             },
         ],
