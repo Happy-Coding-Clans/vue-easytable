7d73125a5621201a3ed0d5e268ffe8fcb2cc131b
huangshuwei <huangsw89520@163.com>
2021-09-06 13:53:14 +0800

    Official documents support IE11

--- examples/babel.config.js
+++ examples/babel.config.js
@@ -9,6 +9,10 @@ module.exports = {
                 corejs: 3,
                 targets: {
                     ie: "11",
+                    edge: "17",
+                    firefox: "60",
+                    chrome: "67",
+                    safari: "11.1",
                 },
                 jsx: {
                     injectH: false,
