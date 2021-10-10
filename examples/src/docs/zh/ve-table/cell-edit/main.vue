35526fc38d068e8e99080244115b4d313f9477ec
huangshuwei <huangsw89520@163.com>
2021-10-10 17:53:34 +0800

    完善示例

--- examples/src/docs/zh/ve-table/cell-edit/main.vue
+++ examples/src/docs/zh/ve-table/cell-edit/main.vue
@@ -3,6 +3,7 @@
         <h2>单元格编辑</h2>
         <Explain />
         <Base />
+        <FullRowWidt />
 
         <!-- <API title="API" anchor="API" desc="filterCustom 自定义筛选配置" /> -->
     </div>
@@ -10,12 +11,14 @@
 <script>
 import Explain from "./explain.md";
 import Base from "./base.md";
+import FullRowWidt from "./full-row-edit.md";
 
 export default {
     name: "basic-main",
     components: {
         Explain,
         Base,
+        FullRowWidt,
     },
 };
 </script>
