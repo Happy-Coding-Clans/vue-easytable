cd2f29553b92f84f65955141f8b2d88c9abb3abe
huangshuwei <huangsw89520@163.com>
2022-01-05 23:01:44 +0800

    Update index.jsx

--- packages/ve-contextmenu/src/index.jsx
+++ packages/ve-contextmenu/src/index.jsx
@@ -258,7 +258,7 @@ export default {
                     });
                 });
             }
-        }, 350),
+        }, 300),
 
         // create panels option
         createPanelOptions({ options, currentMenu }) {
