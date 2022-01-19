1c080cc81e527f1f7e4ae89daaa635cc08f23a4b
huangshuwei <huangsw89520@163.com>
2022-01-19 21:08:22 +0800

    Update index.js

--- examples/src/service/index.js
+++ examples/src/service/index.js
@@ -5,11 +5,18 @@ export function getVersions() {
     return new Promise((resolve, reject) => {
         let net = window.location;
 
+        const isDev = process.env.NODE_ENV === "development";
+
+        let url = `${net.protocol}//${net.hostname}:${net.port}`;
+
         // 包含 /vue-easytable/ 目录
+        if (!isDev) {
+            url += "/vue-easytable";
+        }
+        url += `/versions.json?t=${new Date().getTime()}`;
+
         httpRequest({
-            url: `${net.protocol}//${net.hostname}:${
-                net.port
-            }/vue-easytable/versions.json?t=${new Date().getTime()}`,
+            url: url,
             method: "get",
         })
             .then((res) => {
