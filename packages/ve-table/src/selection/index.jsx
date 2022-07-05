857c4ec06096172b0864068fafcfc6acddb7aeef
huangshuwei <huangsw89520@163.com>
2022-07-05 09:27:27 +0800

    Update index.jsx

--- packages/ve-table/src/selection/index.jsx
+++ packages/ve-table/src/selection/index.jsx
@@ -443,7 +443,7 @@ export default {
                         });
 
                         Object.assign(
-                            this.cellSelectionRect.normalEndCellRect,
+                            this.cellSelectionRect.currentCellRect,
                             someRect,
                         );
                     }
