0f40177c36f977682c9edf9616728209ff872b1f
huangshuwei <huangsw89520@163.com>
2022-07-25 10:32:01 +0800

    更新文档

--- examples/src/docs/zh/ve-table/clipboard/main.vue
+++ examples/src/docs/zh/ve-table/clipboard/main.vue
@@ -2,17 +2,20 @@
     <div>
         <h2>复制和粘贴</h2>
         <Explain />
+        <ShortCuts />
         <Base />
     </div>
 </template>
 <script>
 import Explain from "./explain.md";
+import ShortCuts from "./shortcuts.md";
 import Base from "./base.md";
 
 export default {
     name: "basic-main",
     components: {
         Explain,
+        ShortCuts,
         Base,
     },
 };
