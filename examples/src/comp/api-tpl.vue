aac039cd819ed6ed5e3efc3228b0b7a52e847282
huangshuwei <huangsw89520@163.com>
2022-08-04 20:31:54 +0800

    Update api-tpl.vue

--- examples/src/comp/api-tpl.vue
+++ examples/src/comp/api-tpl.vue
@@ -48,7 +48,7 @@ export default {
         return {
             cellSelectionOption: {
                 // default true
-                enable: true,
+                enable: false,
             },
             cloneTable: [],
         };
