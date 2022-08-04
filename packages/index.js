2fa11a4b549a3617769a63ff626cf3f2fc748fdd
huangshuwei <huangsw89520@163.com>
2022-08-04 20:08:11 +0800

    2.18.0-beta.7

--- packages/index.js
+++ packages/index.js
@@ -13,7 +13,7 @@ import VeSelect from './ve-select';
 import VeTable from './ve-table';
 
 
-const version = '2.18.0-beta.6';
+const version = '2.18.0-beta.7';
 const components = [
   VeCheckbox,
   VeCheckboxGroup,
