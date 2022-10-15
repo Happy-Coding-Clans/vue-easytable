fb0a46e57a826340c2d9cd47407451e7453718d2
huangshuwei <huangsw89520@163.com>
2022-10-15 18:44:57 +0800

    doc refine

--- examples/src/docs/zh/ve-table/cell-autofill/main.vue
+++ examples/src/docs/zh/ve-table/cell-autofill/main.vue
@@ -1,12 +1,14 @@
 <template>
     <div>
         <h2>单元格自动填充</h2>
+        <Explain />
         <Base />
         <AutofillDirection />
         <API title="API" anchor="API" />
     </div>
 </template>
 <script>
+import Explain from "./explain.md";
 import Base from "./base.md";
 import AutofillDirection from "./autofill-direction.md";
 import API from "../api/cell-autofill-option-props";
@@ -14,6 +16,7 @@ import API from "../api/cell-autofill-option-props";
 export default {
     name: "cell-selection",
     components: {
+        Explain,
         Base,
         AutofillDirection,
         API,
