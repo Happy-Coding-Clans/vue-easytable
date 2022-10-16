1b6eb195acd7531309f89db691c1fe30baae9db4
huangshuwei <huangsw89520@163.com>
2022-10-16 11:54:33 +0800

    Update index.jsx

--- packages/ve-table/src/colgroup/index.jsx
+++ packages/ve-table/src/colgroup/index.jsx
@@ -7,13 +7,24 @@ export default {
             type: Array,
             required: true,
         },
+        enableColumnResize: {
+            type: Boolean,
+            required: true,
+        },
     },
     methods: {
         getValByUnit(item) {
-            // 解决使用 _realTimeWidth 在多表头下宽度计算异常的问题
-            return getValByUnit(
-                item._realTimeWidth ? item._realTimeWidth : item.width,
-            );
+            const { enableColumnResize } = this;
+
+            let width;
+            // 开启 column resize
+            if (enableColumnResize) {
+                // 解决使用 _realTimeWidth 在多表头下宽度计算异常的问题
+                width = item._realTimeWidth ? item._realTimeWidth : item.width;
+            } else {
+                width = item.width;
+            }
+            return getValByUnit(width);
         },
     },
     render() {
